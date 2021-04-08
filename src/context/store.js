b696d0f972fb0555011e4fa74e6d3083d0921bba
Yellow May <kwerep09@yahoo.com>
2021-04-08 22:53:14 +0100

    - Context store fixed - Store Token reducer funcion added

--- src/context/store.js
+++ src/context/store.js
@@ -1,14 +1,19 @@
 import React, { createContext, useReducer } from "react";
-import { mainReducer } from "./reducers/mainReducer";
+import mainReducer from "./mainReducer";
 
-const initialState = {};
+const initialState = {
+	username: "",
+	token: "",
+};
 
-export const AppStore = createContext({});
+export const AppContext = createContext({});
 
 export const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(mainReducer, initialState);
 
-	const events = {};
+	const storeToken = payload => dispatch({ type: "STORE_TOKEN", payload });
+
+	const events = { storeToken };
 
 	return (
 		<AppContext.Provider value={{ state, events }}>
--- src/pages/Dashboard/Index.js
+++ src/pages/Dashboard/Index.js
@@ -1,79 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import Chart from 'chart.js';
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState, useEffect, useContext } from "react";
+import Chart from "chart.js";
+import { Container, Row, Col } from "reactstrap";
+// import { AppContext } from "./../../context/store";
 
 // core components
-import { chartOptions, parseOptions } from 'variables/charts.js';
+import { chartOptions, parseOptions } from "variables/charts.js";
 
-import Header from 'components/Dashboard/Header';
-import SummaryCards from 'components/Dashboard/SummaryCards';
-import EmployeeCard from 'components/Dashboard/EmployeeCard';
-import PerformanceChart from 'components/Dashboard/PerformanceChart';
-import RevenueChart from 'components/Dashboard/RevenueChart';
+import Header from "components/Dashboard/Header";
+import SummaryCards from "components/Dashboard/SummaryCards";
+import EmployeeCard from "components/Dashboard/EmployeeCard";
+import PerformanceChart from "components/Dashboard/PerformanceChart";
+import RevenueChart from "components/Dashboard/RevenueChart";
 
 const Index = () => {
-  const [activeNav, setActiveNav] = useState(1);
-  const [chartExample1Data, setChartExample1Data] = useState('data1');
-  const [statistics, setStatistics] = useState({});
+	// const { state, events } = useContext(AppContext);
+	// console.log(state, events);
+	// uncomment import of AppContext and the two above to
+	// view the states or retrieve reducer functions from the events
+	// the state and events are both objects so you can call their keys to retrieve their value pairs
 
-  const url = 'https://localhost:44333/api/statistics';
+	const [activeNav, setActiveNav] = useState(1);
+	const [chartExample1Data, setChartExample1Data] = useState("data1");
+	const [statistics, setStatistics] = useState({});
 
-  if (window.Chart) parseOptions(Chart, chartOptions());
+	const url = "https://localhost:44333/api/statistics";
 
-  const toggleNavs = (e, index) => {
-    e.preventDefault();
-    setActiveNav(index);
-    setChartExample1Data('data' + index);
-  };
+	if (window.Chart) parseOptions(Chart, chartOptions());
 
-  useEffect(() => {
-    const fetchStatistics = async () => {
-      try {
-        const data = await fetch(url).then(res => res.json());
-        setStatistics(data);
-        console.log({ data });
-      } catch (error) {
-        console.log({ error });
-      }
-    };
-    fetchStatistics();
-  }, []);
+	const toggleNavs = (e, index) => {
+		e.preventDefault();
+		setActiveNav(index);
+		setChartExample1Data("data" + index);
+	};
 
-  const { departments, employees } = statistics;
+	useEffect(() => {
+		const fetchStatistics = async () => {
+			try {
+				const data = await fetch(url).then(res => res.json());
+				setStatistics(data);
+				console.log({ data });
+			} catch (error) {
+				console.log({ error });
+			}
+		};
+		fetchStatistics();
+	}, []);
 
-  return (
-    <>
-      <Header employeeCount={employees} departmentCount={departments} />
-      {/* Page content */}
-      <Container className="mt--7" fluid>
-        <Row>
-          <Col className="mb-5 mb-xl-0" xl="8">
-            <RevenueChart
-              activeNav={activeNav}
-              chartExample1Data={chartExample1Data}
-              toggleNavs={toggleNavs}
-            />
-          </Col>
-          <Col xl="4">
-            <PerformanceChart />
-          </Col>
-        </Row>
+	const { departments, employees } = statistics;
 
-        <Row className="mt-5">
-          <Col md={12}>
-            <SummaryCards overallEmployees={employees} />
-          </Col>
-        </Row>
+	return (
+		<>
+			<Header employeeCount={employees} departmentCount={departments} />
+			{/* Page content */}
+			<Container className='mt--7' fluid>
+				<Row>
+					<Col className='mb-5 mb-xl-0' xl='8'>
+						<RevenueChart
+							activeNav={activeNav}
+							chartExample1Data={chartExample1Data}
+							toggleNavs={toggleNavs}
+						/>
+					</Col>
+					<Col xl='4'>
+						<PerformanceChart />
+					</Col>
+				</Row>
 
-        {/* Employee card with table */}
-        <Row className="mt-5 justify-content-center">
-          <Col className="mb-5 mb-xl-0">
-            <EmployeeCard />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+				<Row className='mt-5'>
+					<Col md={12}>
+						<SummaryCards overallEmployees={employees} />
+					</Col>
+				</Row>
+
+				{/* Employee card with table */}
+				<Row className='mt-5 justify-content-center'>
+					<Col className='mb-5 mb-xl-0'>
+						<EmployeeCard />
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default Index;
