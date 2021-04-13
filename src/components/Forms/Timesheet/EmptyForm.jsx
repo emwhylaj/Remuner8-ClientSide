5209bfc299df26f13931e37f36fb6f80b325045b
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-04-13 15:17:11 +0100

    Timesheet page partly done

---
+++ src/components/Forms/Timesheet/EmptyForm.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Col, FormGroup, Input } from 'reactstrap';
+import DatePicker from '../DatePicker';
+import SelectBox from '../SelectBox';
+
+
+const EmptyForm = ({employees}) => {
+    return (
+        <>
+        <Col xs={12}>
+          <FormGroup>
+              <label className="col-form-label">
+                Select Employee <span className="text-danger">*</span>
+              </label>
+              <SelectBox options={employees} />
+            </FormGroup>
+        </Col>
+
+<Col xs={12}>
+            <FormGroup>
+              <label for="exampleDate">
+                Time In<span className="text-danger">*</span>
+              </label>
+          <Input type="number"/>
+            </FormGroup>
+</Col>
+
+<Col xs={12}>
+            <FormGroup>
+              <label for="exampleDate">
+                Time Out<span className="text-danger">*</span>
+              </label>
+              <Input type="number"  />
+            </FormGroup>
+</Col>
+
+<Col>
+            <FormGroup>
+              <label for="description">
+                Hours Worked<span className="text-danger">*</span>
+              </label>
+              <Input
+               
+                name="description"
+                />
+            </FormGroup>  
+                </Col>
+        </>
+    )
+}
+
+export default EmptyForm
---
+++ src/components/Forms/Timesheet/TimesheetForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { FormGroup, Input } from 'reactstrap';
+import CustomForm from 'components/Forms/CustomForm';
+import DatePicker from '../DatePicker';
+import SelectBox from '../SelectBox';
+import EmptyForm from './EmptyForm.jsx';
+
+const employees = ['John Doe', 'Woro Master'];
+
+const TimesheetForm = ({ data, toggle }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    toggle();
+  };
+  return (
+    <CustomForm toggleModal={toggle} handleSubmit={handleSubmit}>
+      {data ? (
+        data.map(overtime => (
+          <>
+            <FormGroup>
+              <label className="col-form-label">
+                Select Employee <span className="text-danger">*</span>
+              </label>
+              <SelectBox options={employees} defaultValue={overtime.name} />
+            </FormGroup>
+
+            <FormGroup>
+              <label for="exampleDate">
+                Overtime Date <span className="text-danger">*</span>
+              </label>
+              <DatePicker date={overtime.otDate} />
+            </FormGroup>
+
+            <FormGroup>
+              <label for="exampleDate">
+                Overtime Hours <span className="text-danger">*</span>
+              </label>
+              <Input type="number" defaultValue={overtime.otHours} />
+            </FormGroup>
+
+            <FormGroup>
+              <label for="description">
+                Description<span className="text-danger">*</span>
+              </label>
+              <Input
+                type="textarea"
+                name="description"
+                defaultValue={overtime.description}
+              />
+            </FormGroup>
+          </>
+        ))
+      ) : (
+        <EmptyForm employees={employees} />
+      )}
+    </CustomForm>
+  );
+};
+
+export default TimesheetForm;
